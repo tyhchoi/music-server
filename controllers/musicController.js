ccc550cc6cfb85389637baba771b8f346377be2f
tyhchoi <tyhchoi@gmail.com>
2018-03-08 00:46:48 -0800

    moved readDir to helpers

--- controllers/musicController.js
+++ controllers/musicController.js
@@ -1,52 +1,37 @@
-const fs = require( 'fs' );
 const path = require( 'path' );
-
-const readDir = ( checkDir, subDirectory = '' ) => {
-  const musicDirectory = process.env.NODE_ENV === 'testing' ? 'music' : process.env.MUSIC_DIRECTORY;
-  const joinedPath = path.join( musicDirectory, subDirectory );
-  const dir = fs.readdirSync( joinedPath );
-
-  const contains = dir.filter( file => file.includes( 'flac' ) );
-
-  if ( checkDir ) {
-    if ( contains.length > 0 ) {
-      return [];
-    }
-    return dir.filter( file => fs.lstatSync( path.join( joinedPath, file ) ).isDirectory() );
-  }
-  return contains;
-};
+const musicFiles = require( '../helpers/musicFiles' );
 
 exports.getArtists = ( req, res ) => {
-  const artists = readDir( true );
+  const artists = musicFiles.readDir( true );
   res.render( 'artists', { title: 'Artists', artists } );
 };
 
 exports.getAlbums = ( req, res ) => {
   const { artist } = req.params;
-  const albums = readDir( true, artist );
+  const albums = musicFiles.readDir( true, artist );
   res.render( 'albums', { artist, albums } );
 };
 
 exports.isCD = ( req, res, next ) => {
   const { artist, album } = req.params;
   const joinedPath = path.join( artist, album );
-  const cds = readDir( true, joinedPath );
+  const cds = musicFiles.readDir( true, joinedPath );
   if ( cds.length > 0 ) {
     res.render( 'cds', { artist, album, cds } );
+  } else {
+    res.locals.songs = musicFiles.readDir( false, joinedPath );
+    next();
   }
-
-  req.params.songs = readDir( false, joinedPath );
-  next();
 };
 
 exports.getSongs = ( req, res ) => {
-  const { artist, album, songs } = req.params;
+  const { artist, album } = req.params;
+  const { songs } = res.locals;
   if ( songs ) {
     res.render( 'songs', { artist, album, songs } );
   } else {
     const { cd } = req.params;
-    const cdSongs = readDir( false, path.join( artist, album, cd ) );
+    const cdSongs = musicFiles.readDir( false, path.join( artist, album, cd ) );
     res.render( 'songs', {
       artist,
       album,
---
+++ helpers/musicFiles.js
@@ -0,0 +1,19 @@
+const fs = require( 'fs' );
+const path = require( 'path' );
+
+exports.readDir = ( checkDir, subDirectory = '' ) => {
+  console.log( 'asdf' );
+  const musicDirectory = process.env.NODE_ENV === 'test' ? 'music' : process.env.MUSIC_DIRECTORY;
+  const joinedPath = path.join( musicDirectory, subDirectory );
+  const dir = fs.readdirSync( joinedPath );
+
+  const contains = dir.filter( file => file.includes( 'flac' ) );
+
+  if ( checkDir ) {
+    if ( contains.length > 0 ) {
+      return [];
+    }
+    return dir.filter( file => fs.lstatSync( path.join( joinedPath, file ) ).isDirectory() );
+  }
+  return contains;
+};
